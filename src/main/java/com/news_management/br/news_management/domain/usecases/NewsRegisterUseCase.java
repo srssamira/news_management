44c53cdfadc54a03fc8654738d5574dbc1134608
Jaiany <jaianyoliveira031@gmail.com>
2024-12-10 16:27:19 -0300

    add dependence injection

--- src/main/java/com/news_management/br/news_management/domain/usecases/NewsRegisterUseCase.java
+++ src/main/java/com/news_management/br/news_management/domain/usecases/NewsRegisterUseCase.java
@@ -1,4 +1,10 @@
 package com.news_management.br.news_management.domain.usecases;
 
+import com.news_management.br.news_management.domain.models.NewsItem;
+
+import java.util.Optional;
+
 public interface NewsRegisterUseCase {
+
+
 }
--- src/main/java/com/news_management/br/news_management/domain/usecases/impl/NewsRegisterUseCaseImpl.java
+++ src/main/java/com/news_management/br/news_management/domain/usecases/impl/NewsRegisterUseCaseImpl.java
@@ -1,6 +1,17 @@
 package com.news_management.br.news_management.domain.usecases.impl;
 
+import com.news_management.br.news_management.domain.dtos.NewsRegisterDTO;
+import com.news_management.br.news_management.domain.models.NewsItem;
 import com.news_management.br.news_management.domain.usecases.NewsRegisterUseCase;
+import java.time.LocalDate;
 
 public class NewsRegisterUseCaseImpl implements NewsRegisterUseCase {
+    private final NewsItem newsItemRepository;
+
+    //injeçao de dependencia
+    public NewsRegisterUseCaseImpl(NewsItem newsItem) {
+        this.newsItemRepository = newsItem;
+    }
+
 }
+
