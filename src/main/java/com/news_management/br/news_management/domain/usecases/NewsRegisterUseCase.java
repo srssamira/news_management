0beda9995b63ac16741c67ee6bb0240dc43e3e30
Jaiany Oliveira Silva <131549653+JaianyOS@users.noreply.github.com>
2024-12-10 16:43:24 -0300

    Revert "By jaianyy"

--- src/main/java/com/news_management/br/news_management/domain/usecases/NewsRegisterUseCase.java
+++ src/main/java/com/news_management/br/news_management/domain/usecases/NewsRegisterUseCase.java
@@ -1,15 +1,4 @@
 package com.news_management.br.news_management.domain.usecases;
 
-import com.news_management.br.news_management.domain.models.NewsItem;
-
-import java.util.Optional;
-
 public interface NewsRegisterUseCase {
-
-    //buscar id
-    Optional<NewsItem> findById(Long id);
-
-    //salvar noticia
-    NewsItem save(NewsItem newsItem);
-
 }
