32aacb92341b64e0b89332b539563c1ab131da27
Samira Rodrigues Silva <srs.samira.silva@gmail.com>
2024-12-10 13:40:17 -0300

    Revert "Adding the Business Rule for Verifying a News Story"
    This reverts commit 5bdae10d1f7c9d5daf649ace231906ebc62ede4d.

--- src/main/java/com/news_management/br/news_management/domain/usecases/NewsVerificatoinUseCase.java
+++ src/main/java/com/news_management/br/news_management/domain/usecases/NewsVerificatoinUseCase.java
@@ -1,10 +1,4 @@
 package com.news_management.br.news_management.domain.usecases;
 
-
-import com.news_management.br.news_management.domain.dtos.NewsVerificationRequestDTO;
-import com.news_management.br.news_management.domain.dtos.NewsVerificationResponseDTO;
-
 public interface NewsVerificatoinUseCase {
-    NewsVerificationResponseDTO verifyNews(NewsVerificationRequestDTO verificationRequest, Long id);
-
 }
--- src/main/java/com/news_management/br/news_management/domain/usecases/impl/NewsVerificationUseCaseImpl.java
+++ src/main/java/com/news_management/br/news_management/domain/usecases/impl/NewsVerificationUseCaseImpl.java
@@ -1,50 +1,6 @@
 package com.news_management.br.news_management.domain.usecases.impl;
 
-import com.news_management.br.news_management.domain.dtos.NewsVerificationRequestDTO;
-import com.news_management.br.news_management.domain.dtos.NewsVerificationResponseDTO;
 import com.news_management.br.news_management.domain.usecases.NewsVerificatoinUseCase;
 
 public class NewsVerificationUseCaseImpl implements NewsVerificatoinUseCase {
-
-    @Override
-    public NewsVerificationResponseDTO verifyNews(NewsVerificationRequestDTO verificationRequest, Long id) {
-
-       int score = 0;
-       String classification;
-
-        if (verificationRequest.isHaveCommunicationVehicle()) {
-            score += 3;
-        }
-
-        if (verificationRequest.isHaveAuthor()) {
-            score += 2;
-        }
-
-        if (verificationRequest.isHavePublicationDate()) {
-            score += 1;
-        }
-
-        if (verificationRequest.isHaveTrustedSource()) {
-            score += 5;
-        } else {
-            score -= 3;
-        }
-
-        if (verificationRequest.isHaveSensacionalistLanguage()) {
-            score -= 3;
-        }
-
-
-        if (score >= 8) {
-            classification = "HIGH CONFIDENCE";
-        } else if (score >= 3) {
-            classification = "MEDIUM CONFIDENCE";
-        } else if (score >= -2) {
-            classification = "LOW CONFIDENCE";
-        } else {
-            classification = "HIGH SUSPICION";
-        }
-
-        return new NewsVerificationResponseDTO(classification,score, verificationRequest.getUrl());
-    }
 }
