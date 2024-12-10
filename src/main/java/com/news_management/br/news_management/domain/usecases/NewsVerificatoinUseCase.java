5bdae10d1f7c9d5daf649ace231906ebc62ede4d
Alicia Feliciano <kasymiryo@gmail.com>
2024-12-10 11:12:16 -0300

    Adding the Business Rule for Verifying a News Story

--- src/main/java/com/news_management/br/news_management/domain/usecases/NewsVerificatoinUseCase.java
+++ src/main/java/com/news_management/br/news_management/domain/usecases/NewsVerificatoinUseCase.java
@@ -1,4 +1,10 @@
 package com.news_management.br.news_management.domain.usecases;
 
+
+import com.news_management.br.news_management.domain.dtos.NewsVerificationRequestDTO;
+import com.news_management.br.news_management.domain.dtos.NewsVerificationResponseDTO;
+
 public interface NewsVerificatoinUseCase {
+    NewsVerificationResponseDTO verifyNews(NewsVerificationRequestDTO verificationRequest, Long id);
+
 }
