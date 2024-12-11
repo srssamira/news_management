9436f57106624bfdba86dd3d30964eac1f4ba960
Alicia Feliciano <kasymiryo@gmail.com>
2024-12-10 22:57:11 -0300

    Raising the business rule for categorizing a news story (NewsVerificationUseCase)

--- src/main/java/com/news_management/br/news_management/domain/dtos/NewsVerificationResponseDTO.java
+++ src/main/java/com/news_management/br/news_management/domain/dtos/NewsVerificationResponseDTO.java
@@ -1,4 +1,44 @@
 package com.news_management.br.news_management.domain.dtos;
 
 public class NewsVerificationResponseDTO {
+
+    private Long id;
+    private String classification;
+    private int score;
+    private String url;
+
+    public NewsVerificationResponseDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
