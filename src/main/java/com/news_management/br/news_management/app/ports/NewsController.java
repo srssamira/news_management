20fa927fe6e942bc5a39a8ce6d7ccd378e13a529
Samira Rodrigues Silva <srs.samira.silva@gmail.com>
2024-12-12 15:34:07 -0300

    add controller SearchAdapter and resolve test of SearchAdapter

--- src/main/java/com/news_management/br/news_management/app/ports/NewsController.java
+++ src/main/java/com/news_management/br/news_management/app/ports/NewsController.java
@@ -1,4 +1,32 @@
 package com.news_management.br.news_management.app.ports;
 
+import com.news_management.br.news_management.app.adapters.NewsItemSearchAdapter;
+import com.news_management.br.news_management.app.infra.NewsNotFoundException;
+import com.news_management.br.news_management.domain.dtos.NewsItemSearchDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/news")
 public class NewsController {
+
+    @Autowired
+    NewsItemSearchAdapter newsItemSearchAdapter;
+
+    @GetMapping("/search")
+    public ResponseEntity<?> searchNews(@RequestParam("keyword") String keyword) {
+        try {
+            NewsItemSearchDTO newsItemSearchDTO = newsItemSearchAdapter.findByKeyword(keyword);
+            return ResponseEntity.ok(newsItemSearchDTO);
+        } catch (NewsNotFoundException notFoundException) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "news not found"));
+        }
+    }
 }
