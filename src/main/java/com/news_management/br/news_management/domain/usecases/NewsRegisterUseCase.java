8ec59f77cc6e2e0e3be8cf197a9895d6ff6d6184
Samira Rodrigues Silva <srs.samira.silva@gmail.com>
2024-12-16 15:06:04 -0300

    resolve problems to run program

--- src/main/java/com/news_management/br/news_management/domain/usecases/NewsRegisterUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.news_management.br.news_management.domain.usecases;
-
-import com.news_management.br.news_management.domain.dtos.NewsVerificationRequestDTO;
-import com.news_management.br.news_management.domain.dtos.NewsVerificationResponseDTO;
-
-public interface NewsRegisterUseCase {
-    NewsVerificationResponseDTO verifyNews(NewsVerificationRequestDTO request, String url);
-}
